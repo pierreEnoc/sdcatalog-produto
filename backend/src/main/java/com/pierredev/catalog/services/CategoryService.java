ca795adfad588a6a2a800cbe88db7965a5d2d394
pierreEnoc <pierre_enoc@yahoo.fr>
2020-10-01 11:35:33 -0300

    :sparkles: database seeding

--- backend/src/main/java/com/pierredev/catalog/services/CategoryService.java
+++ backend/src/main/java/com/pierredev/catalog/services/CategoryService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.pierredev.catalog.entities.Category;
 import com.pierredev.catalog.repositories.CategoryRepository;
@@ -14,9 +15,9 @@ public class CategoryService {
 	@Autowired
 	private CategoryRepository categoryRepository; 
 	
+	@Transactional(readOnly = true)
 	public List<Category> findAll() {
 		return categoryRepository.findAll();
-		
 		
 	}
 
