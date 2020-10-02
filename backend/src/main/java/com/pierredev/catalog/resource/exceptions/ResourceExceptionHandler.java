37f52ca12e95e4c447c4c4336264a26d12e409b0
pierreEnoc <pierre_enoc@yahoo.fr>
2020-10-01 23:36:28 -0300

    :sparkles: add put category

--- backend/src/main/java/com/pierredev/catalog/resource/exceptions/ResourceExceptionHandler.java
+++ backend/src/main/java/com/pierredev/catalog/resource/exceptions/ResourceExceptionHandler.java
@@ -2,14 +2,13 @@ package com.pierredev.catalog.resource.exceptions;
 
 import java.time.Instant;
 
+import javax.persistence.EntityNotFoundException;
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
-import com.pierredev.catalog.services.exceptions.EntityNotFoundException;
 
 @ControllerAdvice
 public class ResourceExceptionHandler {
--- backend/src/main/java/com/pierredev/catalog/services/CategoryService.java
+++ backend/src/main/java/com/pierredev/catalog/services/CategoryService.java
@@ -4,6 +4,8 @@ import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
+import javax.persistence.EntityNotFoundException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -11,7 +13,8 @@ import org.springframework.transaction.annotation.Transactional;
 import com.pierredev.catalog.dto.CategoryDTO;
 import com.pierredev.catalog.entities.Category;
 import com.pierredev.catalog.repositories.CategoryRepository;
-import com.pierredev.catalog.services.exceptions.EntityNotFoundException;
+import com.pierredev.catalog.services.exceptions.ResourceNotFoundException;
+
 
 @Service
 public class CategoryService {
@@ -30,16 +33,36 @@ public class CategoryService {
 	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		Optional<Category> obj = categoryRepository.findById(id);
-		Category entity = obj.orElseThrow(() -> new EntityNotFoundException("Entity not found"));
+		Category entity = obj.orElseThrow(() -> new  EntityNotFoundException("Entity not found"));
+		
 		return new CategoryDTO(entity);
 	}
-
+	
+	@Transactional
 	public CategoryDTO inset(CategoryDTO dto) {
 		Category entity = new Category();
 		entity.setName(dto.getName());
 		entity = categoryRepository.save(entity);
 		
 		return new CategoryDTO(entity);
+	}
+	
+	@Transactional
+	public CategoryDTO update(Long id, CategoryDTO dto) {
+		
+		try {
+		Category entity = categoryRepository.getOne(id);
+		entity.setName(dto.getName());
+		entity = categoryRepository.save(entity);
+		
+		return new CategoryDTO(entity);
+	}
+		catch(EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		
+	}
+
+		
 	}
 
 }
