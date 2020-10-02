2577a663121afa3d6f12be0b3539e4997038aea1
pierreEnoc <pierre_enoc@yahoo.fr>
2020-10-01 22:34:14 -0300

    :sparkles: add post category

--- backend/src/main/java/com/pierredev/catalog/services/CategoryService.java
+++ backend/src/main/java/com/pierredev/catalog/services/CategoryService.java
@@ -34,4 +34,12 @@ public class CategoryService {
 		return new CategoryDTO(entity);
 	}
 
+	public CategoryDTO inset(CategoryDTO dto) {
+		Category entity = new Category();
+		entity.setName(dto.getName());
+		entity = categoryRepository.save(entity);
+		
+		return new CategoryDTO(entity);
+	}
+
 }
