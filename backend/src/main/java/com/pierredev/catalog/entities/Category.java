5cdebda790b16c0e38edcf11889f623a90a132ce
pierreEnoc <pierre_enoc@yahoo.fr>
2020-10-02 13:32:24 -0300

    :sparkles: add auditinf data

--- backend/src/main/java/com/pierredev/catalog/entities/Category.java
+++ backend/src/main/java/com/pierredev/catalog/entities/Category.java
@@ -1,11 +1,15 @@
 package com.pierredev.catalog.entities;
 
 import java.io.Serializable;
+import java.time.Instant;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 
@@ -18,6 +22,12 @@ public class Category implements Serializable {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id; 
 	private String name;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant createdAt;
+	
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+	private Instant updatedAt;
 	
 	
 	public Category() {
@@ -50,8 +60,25 @@ public class Category implements Serializable {
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	public Instant getCreatedAt() {
+		return createdAt;
+	}
 
-
+	public Instant getUpdatedAt() {
+		return updatedAt;
+	}
+	
+	@PrePersist
+	public void prePersist() {
+		createdAt = Instant.now();
+	}
+	
+	@PreUpdate
+	public void preUpdate() {
+		updatedAt = Instant.now();
+	}
+		
 	@Override
 	public int hashCode() {
 		final int prime = 31;
@@ -59,7 +86,7 @@ public class Category implements Serializable {
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
 	}
-
+	
 
 	@Override
 	public boolean equals(Object obj) {
--- backend/src/main/java/com/pierredev/catalog/services/CategoryService.java
+++ backend/src/main/java/com/pierredev/catalog/services/CategoryService.java
@@ -7,62 +7,78 @@ import java.util.stream.Collectors;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.pierredev.catalog.dto.CategoryDTO;
 import com.pierredev.catalog.entities.Category;
 import com.pierredev.catalog.repositories.CategoryRepository;
+import com.pierredev.catalog.services.exceptions.DatabaseException;
 import com.pierredev.catalog.services.exceptions.ResourceNotFoundException;
-
 
 @Service
 public class CategoryService {
-	
+
 	@Autowired
-	private CategoryRepository categoryRepository; 
-	
+	private CategoryRepository categoryRepository;
+
 	@Transactional(readOnly = true)
 	public List<CategoryDTO> findAll() {
 		List<Category> list = categoryRepository.findAll();
-		
-	    return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
-		
+
+		return list.stream().map(x -> new CategoryDTO(x)).collect(Collectors.toList());
+
 	}
-    
+
 	@Transactional(readOnly = true)
 	public CategoryDTO findById(Long id) {
 		Optional<Category> obj = categoryRepository.findById(id);
-		Category entity = obj.orElseThrow(() -> new  EntityNotFoundException("Entity not found"));
-		
+		Category entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
+
 		return new CategoryDTO(entity);
 	}
-	
+
 	@Transactional
 	public CategoryDTO inset(CategoryDTO dto) {
 		Category entity = new Category();
 		entity.setName(dto.getName());
 		entity = categoryRepository.save(entity);
-		
+
 		return new CategoryDTO(entity);
 	}
-	
+
 	@Transactional
 	public CategoryDTO update(Long id, CategoryDTO dto) {
-		
+
 		try {
-		Category entity = categoryRepository.getOne(id);
-		entity.setName(dto.getName());
-		entity = categoryRepository.save(entity);
-		
-		return new CategoryDTO(entity);
-	}
-		catch(EntityNotFoundException e) {
+			Category entity = categoryRepository.getOne(id);
+			entity.setName(dto.getName());
+			entity = categoryRepository.save(entity);
+
+			return new CategoryDTO(entity);
+		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException("Id not found " + id);
+
+		}
+
+	}
+
+	public void delete(Long id) {
+		try {
+			categoryRepository.deleteById(id);
+			
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DatabaseException("Id not found " + id);
+			
+		}
 		
 	}
 
-		
-	}
+
 
 }
