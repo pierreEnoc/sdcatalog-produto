c7d604587e26a68a3a88e79067e5b8035b757428
pierreEnoc <pierre_enoc@yahoo.fr>
2020-11-02 20:27:38 -0300

    :sparkles:custom validation response

---
+++ backend/src/main/java/com/pierredev/catalog/resource/exceptions/VAlidationError.java
@@ -0,0 +1,21 @@
+package com.pierredev.catalog.resource.exceptions;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VAlidationError extends StandardError {
+	
+	private static final long serialVersionUID = 1L;
+	
+	private List<FieldMessage> errors = new ArrayList<>();
+
+	public List<FieldMessage> getErros() {
+		return errors;
+	}
+	
+	public void addError(String fieldName, String message) {
+		errors.add(new FieldMessage(fieldName, message));
+	}
+	
+	
+}
