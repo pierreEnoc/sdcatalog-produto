5e278da8ef8ef1d4663c246aaf84f7d30d4fcbad
pierreEnoc <pierre_enoc@yahoo.fr>
2020-12-28 19:42:34 -0300

    :recycle:refactor using @BeforEach

--- backend/src/test/java/com/pierredev/catalog/tests/repositories/ProductRepositoryTests.java
+++ backend/src/test/java/com/pierredev/catalog/tests/repositories/ProductRepositoryTests.java
@@ -3,6 +3,7 @@ package com.pierredev.catalog.tests.repositories;
 import java.util.Optional;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -16,12 +17,21 @@ public class ProductRepositoryTests {
 	@Autowired
 	private ProductRepository repository;
 	
+	private long existingId;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		existingId = 1L;
+	}
+	
+	
+	
 	@Test
 	public void deleteShouldDeleteObjectWhwnIdExists() {
 		
-		repository.deleteById(1L);
+		repository.deleteById(existingId);
 		
-		Optional<Product> result = repository.findById(1L);
+		Optional<Product> result = repository.findById(existingId);
 		
 		Assertions.assertFalse(result.isPresent());
 	}
