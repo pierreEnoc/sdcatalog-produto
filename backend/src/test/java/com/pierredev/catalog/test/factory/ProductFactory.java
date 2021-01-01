61d9bc04c1bee459864fdfeb600dd76b83db610f
pierreEnoc <pierre_enoc@yahoo.fr>
2020-12-31 21:42:54 -0300

    :sparkles:test findById

--- backend/src/test/java/com/pierredev/catalog/test/factory/ProductFactory.java
+++ backend/src/test/java/com/pierredev/catalog/test/factory/ProductFactory.java
@@ -3,16 +3,26 @@ package com.pierredev.catalog.test.factory;
 import java.time.Instant;
 
 import com.pierredev.catalog.dto.ProductDTO;
+import com.pierredev.catalog.entities.Category;
 import com.pierredev.catalog.entities.Product;
 
 public class ProductFactory {
 	
 	public static Product createProduct() {
-		return  new Product(1L, "phone", "Good phone", 800.0, "https://img.com/img.pmg", Instant.parse("2020-11-20T03:00:00z"));
+		Product product = new Product(1L, "phone", "Good phone", 800.0, "https://img.com/img.pmg", Instant.parse("2020-11-20T03:00:00z"));
+		product.getCategories().add(new Category(1L, null));
+		return product;
 	}
 	
 	public static ProductDTO createProductDTO() {
-		return new ProductDTO(createProduct());
+		Product product = createProduct();
+		return new ProductDTO(product, product.getCategories());
+	}
+	
+	public static ProductDTO createProductDTO(Long id) {
+		ProductDTO dto = createProductDTO();
+		dto.setId(id);
+		return dto;
 	}
 
 }
---
+++ backend/src/test/java/com/pierredev/catalog/test/web/ProductResourceTests.java
@@ -0,0 +1,119 @@
+package com.pierredev.catalog.test.web;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.doThrow;
+import static org.mockito.Mockito.when;
+//import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.httpBasic;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+
+import java.util.List;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.json.JacksonJsonParser;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+import com.pierredev.catalog.dto.ProductDTO;
+import com.pierredev.catalog.services.ProductService;
+import com.pierredev.catalog.services.exceptions.ResourceNotFoundException;
+import com.pierredev.catalog.test.factory.ProductFactory;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class ProductResourceTests {
+
+	@Autowired
+	private MockMvc mockMvc;
+	
+	@MockBean
+	private ProductService service;
+
+	@Value("${security.oauth2.client.client-id}")
+	private String clientId;
+	
+	@Value("${security.oauth2.client.client-secret}")
+	private String clientSecret;
+	
+	private Long existingId;
+	private Long nonExistingId;
+	private ProductDTO newProductDTO;
+	private ProductDTO existingProductDTO;
+	
+	@BeforeEach
+	void setup() throws Exception {
+		existingId = 1L;
+		nonExistingId = 2L;
+		
+		newProductDTO = ProductFactory.createProductDTO(null);
+		existingProductDTO = ProductFactory.createProductDTO(existingId);
+
+		Mockito.when(service.findById(existingId)).thenReturn(existingProductDTO);
+		Mockito.when(service.findById(nonExistingId)).thenThrow(ResourceNotFoundException.class);
+	}	
+	
+	@Test
+	public void findByIdShouldReturnProductWhenIdExists() throws Exception {
+		ResultActions result = 
+				mockMvc.perform(get("/products/{id}", nonExistingId)
+						.accept(MediaType.APPLICATION_JSON));
+		
+				result.andExpect(status().isNotFound());
+	}
+	
+	@Test
+	public void findByIdShouldReturnProductWhenIdDoesNotExist() throws Exception  {
+		
+		ResultActions result = 
+				mockMvc.perform(get("/products/{id}", existingId)
+						.accept(MediaType.APPLICATION_JSON));
+		
+				result.andExpect(status().isOk());
+		
+	}
+	
+	private String obtainAccessToken(String username, String password) throws Exception {
+		 
+	    MultiValueMap<String, String> params = new LinkedMultiValueMap<>();
+	    params.add("grant_type", "password");
+	    params.add("client_id", clientId);
+	    params.add("username", username);
+	    params.add("password", password);
+	 
+	    ResultActions result 
+	    	= mockMvc.perform(post("/oauth/token")
+	    		.params(params)
+	    		//.with(httpBasic(clientId, clientSecret))
+	    		.accept("application/json;charset=UTF-8"))
+	        	.andExpect(status().isOk())
+	        	.andExpect(content().contentType("application/json;charset=UTF-8"));
+	 
+	    String resultString = result.andReturn().getResponse().getContentAsString();
+	 
+	    JacksonJsonParser jsonParser = new JacksonJsonParser();
+	    return jsonParser.parseMap(resultString).get("access_token").toString();
+	}	
+
+		
+}
